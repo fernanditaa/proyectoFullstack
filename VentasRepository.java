52f80599cb33b9365eed1e2f30e3a624facc3978
fernanditaa <163853793+fernanditaa@users.noreply.github.com>
2025-06-05 16:54:26 -0400

    Este tiene Query

--- VentasRepository.java
+++ VentasRepository.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.NoLimits.Multimedia.model.VentasModel;
@@ -12,9 +13,14 @@ import com.example.NoLimits.Multimedia.model.VentasModel;
 @Repository
 public interface VentasRepository extends JpaRepository<VentasModel, Long>{
 
+    @Query("""
+            
+            SELECT v.id, v.nombre, v.fechaCompra, v.horaCompra, v.totalVenta FROM VentasModel v
+            """)
+
     List<VentasModel> findByFechaCompra(LocalDate FechaCompra);
 
-    VentasModel findByHoracompra(LocalTime HoraCompra);
+    VentasModel findByHoraCompra(LocalTime HoraCompra);
 
     VentasModel findById(Integer id);
 }
