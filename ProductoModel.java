891e23903ee16af1cc4a07456c7db353c1f9372d
fernanditaa <163853793+fernanditaa@users.noreply.github.com>
2025-06-08 17:07:01 -0400

    Add files via upload

--- ProductoModel.java
+++ ProductoModel.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -33,4 +35,7 @@ public class ProductoModel {
     @Column(nullable=false)
     private Integer stock;
 
+    @ManyToOne
+    @JoinColumn(name="estadoVenta_id")
+    private EstadoDeVenta estadoDeVenta;
 }
--- ProductoRepository.java
+++ ProductoRepository.java
@@ -3,6 +3,7 @@ package com.example.NoLimits.Multimedia.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.NoLimits.Multimedia.model.ProductoModel;
@@ -11,6 +12,10 @@ import com.example.NoLimits.Multimedia.model.ProductoModel;
 @Repository
 public interface ProductoRepository extends JpaRepository<ProductoModel, Long> {
     
+    @Query("""
+            SELECT p FROM ProductoModel p
+            
+            """)
     List<ProductoModel> findByNombre(String nombre);
     
     ProductoModel findById(Integer id);
